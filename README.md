bb93eae7a2f963608a367598b648cc095391e482
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-11-29 12:40:36 -0500

    se agrego el ejercicio 01

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # trabajo05
 Trabajo Nro 5
+Llontop Atencio Ena
+Rufasto Torres Diana
---
+++ t05_Llontop_Atencio.py
@@ -0,0 +1,12 @@
+#EJERCICIO_01
+monto_total_de_un_empleado=548.8
+type_monto_total_de_un_empleado=type(monto_total_de_un_empleado)
+
+monto_total_de_un_empleado_es_float=isinstance(monto_total_de_un_empleado,float)
+monto_total_de_un_empleado_es_str=isinstance(monto_total_de_un_empleado,str)
+
+print("variable monto total de un empleado es:",type_monto_total_de_un_empleado)
+print("variable es float?:",monto_total_de_un_empleado_es_float)
+print("variable es str?:",monto_total_de_un_empleado_es_str)
+
+
