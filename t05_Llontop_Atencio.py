7e353ea218727fbe65815ac817623206b007abea
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-11-29 13:31:56 -0500

    se agrego el ejercicio 02

--- t05_Llontop_Atencio.py
+++ t05_Llontop_Atencio.py
@@ -10,3 +10,13 @@ print("variable es float?:",monto_total_de_un_empleado_es_float)
 print("variable es str?:",monto_total_de_un_empleado_es_str)
 
 
+#EJERCICIO_02
+monto_total_de_un_ganadero=289.60
+type_monto_total_de_un_ganadero=type(monto_total_de_un_ganadero)
+
+monto_total_de_un_ganadero_es_float=isinstance(type_monto_total_de_un_ganadero,float)
+monto_total_de_un_ganadero_es_int=isinstance(type_monto_total_de_un_ganadero,int)
+
+print("variable monto total de un ganadero es:",type_monto_total_de_un_ganadero)
+print("variable es float?:",monto_total_de_un_ganadero_es_float)
+print("variable es int?:",monto_total_de_un_ganadero_es_int)
