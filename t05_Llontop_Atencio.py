480f66444373f1d1a3feabd3b56cfd43f2af07e6
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-11-29 17:24:05 -0500

    se agrego el ejercicio 03

--- t05_Llontop_Atencio.py
+++ t05_Llontop_Atencio.py
@@ -14,9 +14,21 @@ print("variable es str?:",monto_total_de_un_empleado_es_str)
 monto_total_de_un_ganadero=289.60
 type_monto_total_de_un_ganadero=type(monto_total_de_un_ganadero)
 
-monto_total_de_un_ganadero_es_float=isinstance(type_monto_total_de_un_ganadero,float)
-monto_total_de_un_ganadero_es_int=isinstance(type_monto_total_de_un_ganadero,int)
+monto_total_de_un_ganadero_es_float=isinstance(monto_total_de_un_ganadero,float)
+monto_total_de_un_ganadero_es_int=isinstance(monto_total_de_un_ganadero,int)
 
 print("variable monto total de un ganadero es:",type_monto_total_de_un_ganadero)
 print("variable es float?:",monto_total_de_un_ganadero_es_float)
 print("variable es int?:",monto_total_de_un_ganadero_es_int)
+
+
+#EJERCICIO_03
+ingreso_total_de_una_consultora_natura=557.98
+type_ingreso_total_de_una_consultora_natura=type(ingreso_total_de_una_consultora_natura)
+
+ingreso_total_de_una_consultora_natura_es_float=isinstance(ingreso_total_de_una_consultora_natura,float)
+ingreso_total_de_una_consultora_natura_es_str=isinstance(ingreso_total_de_una_consultora_natura,str)
+
+print("variable ingreso total de una consultora natura es:",type_ingreso_total_de_una_consultora_natura)
+print("variable es float?:",ingreso_total_de_una_consultora_natura_es_float)
+print("variable es str?:",ingreso_total_de_una_consultora_natura_es_str)
