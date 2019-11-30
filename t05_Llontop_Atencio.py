d555ed0609f0f6f7ee39baa786635439d73a1286
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-11-30 00:21:02 -0500

    se agrego el ejercicio 24

--- t05_Llontop_Atencio.py
+++ t05_Llontop_Atencio.py
@@ -272,3 +272,15 @@ type_numero_de_visitas_bruning_es_str=isinstance(numero_de_visitas_bruning,str)
 print("variable numero de visitas bruning:",type_numero_de_visitas_bruning_es_int)
 print("variable es int?:",type_numero_de_visitas_bruning_es_int)
 print("variable es str?:",type_numero_de_visitas_bruning_es_str)
+
+
+#EJERCICIO_24
+promedio_de_visitas_a_lugares_turisticos=62
+type_promedio_de_visitas_a_lugares_turisticos=type(promedio_de_visitas_a_lugares_turisticos)
+
+type_promedio_de_visitas_a_lugares_turisticos_es_int=isinstance(promedio_de_visitas_a_lugares_turisticos,int)
+type_promedio_de_visitas_a_lugares_turisticos_es_str=isinstance(promedio_de_visitas_a_lugares_turisticos,str)
+
+print("variable promedio de visitas a lugares turisticos:",type_promedio_de_visitas_a_lugares_turisticos_es_int)
+print("variable es int?:",type_promedio_de_visitas_a_lugares_turisticos_es_int)
+print("variable es str?:",type_promedio_de_visitas_a_lugares_turisticos_es_str)
