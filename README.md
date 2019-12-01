bfce0f6c12122973e6545acafcbdd87c144242b9
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-11-30 19:20:19 -0500

    se agrego el ejercicio 51

--- README.md
+++
@@ -1,4 +0,0 @@
-# trabajo05
-Trabajo Nro 5
-Llontop Atencio Ena
-Rufasto Torres Diana
---
+++ t05_rufasto_torres.py
@@ -0,0 +1,556 @@
+#EJERCICIO01
+promedio_de_un_alumno= 14.05
+type_promedio_de_un_alumno=type(promedio_de_un_alumno)
+
+promedio_de_un_alumno_es_float=isinstance(promedio_de_un_alumno,float)
+promedio_de_un_alumno_es_str=isinstance(promedio_de_un_alumno,str)
+
+print("variable promedio de un alumno es:",type_promedio_de_un_alumno)
+print("variable es float?:",promedio_de_un_alumno_es_float)
+print("variable es str?:",promedio_de_un_alumno_es_str)
+
+#EJERCICIO02
+IMC_con_calificacion_sobrepeso=28.8966
+type_IMC_con_calificacion_sobrepeso=type(IMC_con_calificacion_sobrepeso)
+
+IMC_con_calificacion_sobrepeso_es_float=isinstance(IMC_con_calificacion_sobrepeso,float)
+IMC_con_calificacion_sobrepeso_es_bool=isinstance(IMC_con_calificacion_sobrepeso,bool)
+
+print("variable IMC con calificacion sobrepeso es:",type_IMC_con_calificacion_sobrepeso)
+print("variable es float?:",IMC_con_calificacion_sobrepeso_es_float)
+print("variable es bool?:",IMC_con_calificacion_sobrepeso_es_bool)
+
+#EJERCICIO03
+IMC_con_calificacion_delgadez_severa=14.5684
+type_IMC_con_calificacion_delgadez_severa=type(IMC_con_calificacion_delgadez_severa)
+
+IMC_con_calificacion_delgadez_severa_es_float=isinstance(IMC_con_calificacion_delgadez_severa,float)
+IMC_con_calificacion_delgadez_severa_es_str=isinstance(IMC_con_calificacion_delgadez_severa,str)
+
+print("variable IMC con calificacion delgadez severa:",type_IMC_con_calificacion_delgadez_severa)
+print("variable es float?:",IMC_con_calificacion_delgadez_severa_es_float)
+print("variable es str?:",IMC_con_calificacion_delgadez_severa_es_str)
+
+#EJERCICIO04
+dias_de_vacaciones=15
+type_dias_de_vacaciones=type(dias_de_vacaciones)
+
+dias_de_vacaciones_es_int=isinstance(dias_de_vacaciones,int)
+dias_de_vacaciones_es_float=isinstance(dias_de_vacaciones,float)
+
+print("variable dias de vacaciones es:",type_dias_de_vacaciones)
+print("variable es int?:",dias_de_vacaciones_es_int)
+print("variable es float?:",dias_de_vacaciones_es_float)
+
+#EJERCICIO05
+ocupa_tercio_superior="Jimmy"
+type_ocupa_tercio_superior=type(ocupa_tercio_superior)
+
+ocupa_tercio_superior_es_str=isinstance(ocupa_tercio_superior,str)
+ocupa_tercio_superior_es_float=isinstance(ocupa_tercio_superior,float)
+
+print("variable ocupa tercio superior:",type_ocupa_tercio_superior)
+print("variable es str?:",ocupa_tercio_superior_es_str)
+print("variable es float?:",ocupa_tercio_superior_es_float)
+
+#EJERCICIO06
+monto_de_pasar_pedido=251.45
+type_monto_de_pasar_pedido=type(monto_de_pasar_pedido)
+
+monto_de_pasar_pedido_es_float=isinstance(monto_de_pasar_pedido,float)
+monto_de_pasar_pedido_es_int=isinstance(monto_de_pasar_pedido,int)
+
+
+print("variable monto de pasar pedido es:",type_monto_de_pasar_pedido)
+print("variable es float?:",monto_de_pasar_pedido_es_float)
+print("variable es int?:",monto_de_pasar_pedido_es_int)
+
+#EJERCICIO07
+monto_de_liquidacion=599.50
+type_monto_de_liquidacion=type(monto_de_liquidacion)
+
+monto_de_liquidacion_es_float=isinstance(monto_de_liquidacion,float)
+monto_de_liquidacion_es_bool=isinstance(monto_de_liquidacion,bool)
+
+print("variable monto de liquidacion es:",type_monto_de_liquidacion)
+print("variable es float?:",monto_de_liquidacion_es_float)
+print("variable es bool?:",monto_de_liquidacion_es_bool)
+
+
+#EJERCICIO08
+comprador_compulsivo="Maria"
+type_comprador_compulsivo=type(comprador_compulsivo)
+
+comprador_compulsivo_es_str=isinstance(comprador_compulsivo,str)
+comprador_compulsivo_es_float=isinstance(comprador_compulsivo,float)
+
+print("variable comprador compulsivo es:",type_comprador_compulsivo)
+print("variable es str?:",comprador_compulsivo_es_str)
+print("variable es float?:",comprador_compulsivo_es_float)
+
+
+#EJERCICIO09
+total_a_pagar=345.60
+type_total_a_pagar=type(total_a_pagar)
+
+total_a_pagar_es_float=isinstance(total_a_pagar,float)
+total_a_pagar_es_bool=isinstance(total_a_pagar,bool)
+
+print("variable total a pagar es:",type_total_a_pagar)
+print("variable es float?:",total_a_pagar_es_float)
+print("variable es bool?:",total_a_pagar_es_bool)
+
+#EJERCICIO10
+nro_de_javas=50
+type_nro_de_javas=type(nro_de_javas)
+
+nro_de_javas_es_int=isinstance(nro_de_javas,int)
+nro_de_javas_es_str=isinstance(nro_de_javas,str)
+
+print("variable nro de javas es:",type_nro_de_javas)
+print("variable es int?:",nro_de_javas_es_int)
+print("variable es str?:",nro_de_javas_es_str)
+
+
+#EJERCICIO11
+obrero="Segundo"
+type_obrero=type(obrero)
+
+obrero_es_str=isinstance(obrero,str)
+obrero_es_bool=isinstance(obrero,bool)
+
+print("variable obrero es:",type_obrero)
+print("variable es str?:",obrero_es_str)
+print("variable es bool?:",obrero_es_bool)
+
+
+#EJERCICIO12
+total_de_ajies_por_min=20
+type_total_de_ajies_por_min=type(total_de_ajies_por_min)
+
+total_de_ajies_por_min_es_int=isinstance(total_de_ajies_por_min,int)
+total_de_ajies_por_min_es_float=isinstance(total_de_ajies_por_min,float)
+
+print("variable total de ajies es:",type_total_de_ajies_por_min)
+print("variable es int?:",total_de_ajies_por_min_es_int)
+print("variable es float?:",total_de_ajies_por_min_es_float)
+
+
+#EJERCICIO13
+empresa="Gandules"
+type_empresa=type(empresa)
+
+empresa_es_str=isinstance(empresa,str)
+empresa_es_float=isinstance(empresa,float)
+
+print("variable empresa:",type_empresa)
+print("variable es str?:",empresa_es_str)
+print("variable es float?:",empresa_es_float)
+
+#EJERCICIO14
+nota_de_sustentacion_de_tesis=18.5
+type_nota_de_sustentacion_de_tesis=type(nota_de_sustentacion_de_tesis)
+
+nota_de_sustentacion_de_tesis_es_float=isinstance(nota_de_sustentacion_de_tesis,float)
+nota_de_sustentacion_de_tesis_es_str=isinstance(nota_de_sustentacion_de_tesis,str)
+
+print("variable nota de sustentacion de tesis es:",type_nota_de_sustentacion_de_tesis)
+print("variable es float?:",nota_de_sustentacion_de_tesis_es_float)
+print("variable es str?:",nota_de_sustentacion_de_tesis_es_str)
+
+#EJERCICIO15
+universidad="Pedro Ruis Gallo"
+type_universidad=type(universidad)
+
+universidad_es_str=isinstance(universidad,str)
+universidad_es_float=isinstance(universidad,float)
+
+print("variable universidad es:",type_universidad)
+print("variable es str?:",universidad_es_str)
+print("variable es float?:",universidad_es_float)
+
+#EJERCICIO16
+ingreso_mensual=1500.00
+type_ingreso_mensual=type(ingreso_mensual)
+
+ingreso_mensual_es_float=isinstance(ingreso_mensual,float)
+ingreso_mensual_es_bool=isinstance(ingreso_mensual,bool)
+
+print("variable ingreso mensual es:",type_ingreso_mensual)
+print("variable es float?:",ingreso_mensual_es_float)
+print("variable es bool?:",ingreso_mensual_es_bool)
+
+#EJERCICIO17
+peso_del_paciente=58.5
+type_peso_del_paciente=type(peso_del_paciente)
+
+peso_del_paciente_es_float=isinstance(peso_del_paciente,float)
+peso_del_paciente_es_int=isinstance(peso_del_paciente,int)
+
+print("variable peso del paciente es:",type_peso_del_paciente)
+print("variable es float?:",peso_del_paciente_es_float)
+print("variable es int?:",peso_del_paciente_es_int)
+
+
+#EJERCICIO18
+talla_de_paciente=1.65
+type_talla_de_paciente=type(talla_de_paciente)
+
+talla_de_paciente_es_float=isinstance(talla_de_paciente,float)
+talla_de_paciente_es_str=isinstance(talla_de_paciente,str)
+
+print("variable talla del paciente es:",type_talla_de_paciente)
+print("variable es float?:",talla_de_paciente_es_float)
+print("variable es str?:",talla_de_paciente_es_str)
+
+#EJERCICIO19
+producto="labiales"
+type_producto=type(producto)
+
+producto_es_str=isinstance(producto,str)
+producto_es_bool=isinstance(producto,bool)
+
+print("variable producto es:",type_producto)
+print("variable es str?:",producto_es_str)
+print("variable es bool?:",producto_es_bool)
+
+#EJERCICIO20
+marca_de_celular="iphone"
+type_marca_de_celular=type(marca_de_celular)
+
+marca_de_celular_es_str=isinstance(marca_de_celular,str)
+marca_de_celular_es_int=isinstance(marca_de_celular,int)
+
+print("variable marca de celular es:",type_marca_de_celular)
+print("variable es str?:",marca_de_celular_es_str)
+print("variable es int?:",marca_de_celular_es_int)
+
+#EJERCICIO21
+vendedor="Edgar"
+type_vendedor=type(vendedor)
+
+vendedor_es_str=isinstance(vendedor,str)
+vendedor_es_float=isinstance(vendedor,float)
+
+print("variable vendedor es:",type_vendedor)
+print("variable es str?:",vendedor_es_str)
+print("variable es float?:",vendedor_es_float)
+
+#EJERCICIO22
+cantidad_de_celulares=30
+type_cantidad_de_celulares=type(cantidad_de_celulares)
+
+cantidad_de_celulares_es_int=isinstance(cantidad_de_celulares,int)
+cantidad_de_celulares_es_bool=isinstance(cantidad_de_celulares,bool)
+
+print("variable cantidad de celulares es:",type_cantidad_de_celulares)
+print("variable es int?:",cantidad_de_celulares_es_int)
+print("variable es bool?:",cantidad_de_celulares_es_bool)
+
+#EJERCICIO23
+costo_unitario_celular=1.800
+type_costo_unitario_celular=type(costo_unitario_celular)
+
+costo_unitario_celular_es_float=isinstance(costo_unitario_celular,float)
+costo_unitario_celular_es_str=isinstance(costo_unitario_celular,str)
+
+print("variable costo unitario celular es:",type_costo_unitario_celular)
+print("variable es float?:",costo_unitario_celular_es_float)
+print("variable es str?:",costo_unitario_celular_es_str)
+
+#EJERCICIO24
+clinica_odontologica="Muelitas"
+type_clinica_odontologica=type(clinica_odontologica)
+
+clinica_odontologica_es_str=isinstance(clinica_odontologica,str)
+clinica_odontologica_es_float=isinstance(clinica_odontologica,float)
+
+print("variable clinica odontologica es:",type_clinica_odontologica)
+print("variable es str?:",clinica_odontologica_es_str)
+print("variable es float?:",clinica_odontologica_es_float)
+
+#EJERCICIO25
+asignacion_familiar=26.00
+type_asignacion_familiar=type(asignacion_familiar)
+
+asignacion_familiar_es_float=isinstance(asignacion_familiar,float)
+asignacion_familiar_es_bool=isinstance(asignacion_familiar,bool)
+
+print("variable asignacion familiar es:",type_asignacion_familiar)
+print("variable es float?:",asignacion_familiar_es_float)
+print("variable es bool?:",asignacion_familiar_es_bool)
+
+#EJERCICIO26
+nro_de_dias=27
+type_nro_de_dias=type(nro_de_dias)
+
+nro_de_dias_es_int=isinstance(nro_de_dias,int)
+nro_de_dias_es_str=isinstance(nro_de_dias,str)
+
+print("variable nro de dias es:",type_nro_de_dias)
+print("variable es int?:",nro_de_dias_es_int)
+print("variable es str?:",nro_de_dias_es_str)
+
+#EJERCICIO27
+curso="Computacion"
+type_curso=type(curso)
+
+curso_es_str=isinstance(curso,str)
+curso_es_float=isinstance(curso,float)
+
+print("variable curso es:",type_curso)
+print("variable es str?:",curso_es_str)
+print("variable es float?:",curso_es_float)
+
+#EJERCICIO28
+ciclo="quinto"
+type_ciclo=type(ciclo)
+
+ciclo_es_str=isinstance(ciclo,str)
+ciclo_es_bool=isinstance(ciclo,bool)
+
+print("variable ciclo es:",type_ciclo)
+print("variable es str?:",ciclo_es_str)
+print("variable es bool?:",ciclo_es_bool)
+
+#EJERCICIO29
+total_de_anios=16
+type_total_de_anios=type(total_de_anios)
+
+total_de_anios_es_int=isinstance(total_de_anios,int)
+total_de_anios_es_float=isinstance(total_de_anios,float)
+
+print("variable total de anios es:",type_total_de_anios)
+print("variable es int?:",total_de_anios_es_int)
+print("variable es float?:",total_de_anios_es_float)
+
+#EJERCICIO30
+total_de_meses=9
+type_total_de_meses=type(total_de_meses)
+
+total_de_meses_es_int=isinstance(total_de_meses,int)
+total_de_meses_es_bool=isinstance(total_de_meses,bool)
+
+print("variable total de meses es:",type_total_de_meses)
+print("variable es int?:",total_de_meses_es_int)
+print("variable es bool?:",total_de_meses_es_bool)
+
+#EJERCICIO31
+total_de_dias=17
+type_total_de_dias=type(total_de_dias)
+
+total_de_dias_es_int=isinstance(total_de_dias,int)
+total_de_dias_es_str=isinstance(total_de_dias,str)
+
+print("variable total de dias es:",type_total_de_dias)
+print("variable es int?:",total_de_dias_es_int)
+print("variable es str?:",total_de_dias_es_str)
+
+#EJERCICIO32
+nota_del_primer_curso=16
+type_nota_del_primer_curso=type(nota_del_primer_curso)
+
+nota_del_primer_curso_es_int=isinstance(nota_del_primer_curso,int)
+nota_del_primer_curso_es_float=isinstance(nota_del_primer_curso,float)
+
+print("variable nota del primer curso es:",type_nota_del_primer_curso)
+print("variable es int?:",nota_del_primer_curso_es_int)
+print("variable es float?:",nota_del_primer_curso_es_float)
+
+#EJERCICIO33
+nota_del_segundo_curso=18
+type_nota_del_segundo_curso=type(nota_del_segundo_curso)
+
+nota_del_segundo_curso_es_int=isinstance(nota_del_segundo_curso,int)
+nota_del_segundo_curso_es_str=isinstance(nota_del_segundo_curso,str)
+
+print("variable nota del segundo curso es:",type_nota_del_segundo_curso)
+print("variable es int?:",nota_del_segundo_curso_es_int)
+print("variable es str?:",nota_del_segundo_curso_es_str)
+
+#EJERCICIO34
+nota_de_tercer_curso=14
+type_nota_del_segundo_curso=type(nota_del_segundo_curso)
+
+nota_del_segundo_curso_es_int=isinstance(nota_del_segundo_curso,int)
+nota_del_segundo_curso_es_bool=isinstance(nota_del_segundo_curso,bool)
+
+print("variable nota del tercer curso es:",type_nota_del_segundo_curso)
+print("variable es int?:",nota_del_segundo_curso_es_int)
+print("variable es bool?:",nota_del_segundo_curso_es_bool)
+
+#EJERCICIO35
+horas_trabajadas=8
+type_horas_trabajadas=type(horas_trabajadas)
+
+horas_trabajadas_es_int=isinstance(horas_trabajadas,int)
+horas_trabajadas_es_str=isinstance(horas_trabajadas,str)
+
+print("variable horas trabajadas es:",type_horas_trabajadas)
+print("variable es int?:",horas_trabajadas_es_int)
+print("variable es str?:",horas_trabajadas_es_str)
+
+#EJERCICIO36
+horas_trabajadas_semanal=50
+type_horas_trabajadas_semanal=type(horas_trabajadas_semanal)
+
+horas_trabajadas_semanal_es_int=isinstance(horas_trabajadas_semanal,int)
+horas_trabajadas_semanal_es_float=isinstance(horas_trabajadas_semanal,float)
+
+print("variable horas trabajadas semanal es:",type_horas_trabajadas_semanal)
+print("variable es int?:",horas_trabajadas_semanal_es_int)
+print("variable es float?:",horas_trabajadas_semanal_es_float)
+
+#EJERCICIO37
+salario_de_un_obrero=1.800
+type_salario_de_un_obrero=type(salario_de_un_obrero)
+
+salario_de_un_obrero_es_float=isinstance(salario_de_un_obrero,float)
+salario_de_un_obrero_es_bool=isinstance(salario_de_un_obrero,bool)
+
+print("variable salio de un obrero es:",type_salario_de_un_obrero)
+print("variable es float?:",salario_de_un_obrero_es_float)
+print("variable es bool?:",salario_de_un_obrero_es_bool)
+
+#EJERCICIO38
+ingreso_del_padre=1.500
+type_ingreso_del_padre=type(ingreso_del_padre)
+
+ingreso_del_padre_es_float=isinstance(ingreso_del_padre,float)
+ingreso_del_padre_es_int=isinstance(ingreso_del_padre,int)
+
+print("variable ingreso del padre es:",type_ingreso_del_padre)
+print("variable es float?:",ingreso_del_padre_es_float)
+print("variable es int?:",ingreso_del_padre_es_int)
+
+#EJERCICIO39
+ingreso_de_la_madre=1.200
+type_ingreso_de_la_madre=type(ingreso_de_la_madre)
+
+ingreso_de_la_madre_es_float=isinstance(ingreso_de_la_madre,float)
+ingreso_de_la_madre_es_str=isinstance(ingreso_de_la_madre,str)
+
+print("variable ingreso de la madre es:",type_ingreso_de_la_madre)
+print("variable es float?:",ingreso_de_la_madre_es_float)
+print("variable es str?:",ingreso_de_la_madre_es_str)
+
+#EJERCICIO40
+ingreso_del_hijo=500.00
+type_ingreso_del_hijo=type(ingreso_del_hijo)
+
+ingreso_del_hijo_es_float=isinstance(ingreso_del_hijo,float)
+ingreso_del_hijo_es_bool=isinstance(ingreso_del_hijo,bool)
+
+print("variable ingreso del hijo es:",type_ingreso_del_hijo)
+print("variable es float?:",ingreso_del_hijo_es_float)
+print("variable es bool?:",ingreso_del_hijo_es_bool)
+
+#EJERCICIO41
+ingreso_total=3.500
+type_ingreso_total=type(ingreso_total)
+
+ingreso_total_es_float=isinstance(ingreso_total,float)
+ingreso_total_es_int=isinstance(ingreso_total,int)
+
+print("variable ingreso total es:",type_ingreso_total)
+print("variable es float?:",ingreso_total_es_float)
+print("variable es int?:",ingreso_total_es_int)
+
+#EJERCICIO42
+edad_del_paciente=25
+type_edad_del_paciente=type(edad_del_paciente)
+
+edad_del_paciente_es_int=isinstance(edad_del_paciente,int)
+edad_del_paciente_es_bool=isinstance(edad_del_paciente,bool)
+
+print("variable edad del paciente es:",type_edad_del_paciente)
+print("variable es int?:",edad_del_paciente_es_int)
+print("variable es bool?:",edad_del_paciente_es_bool)
+
+#EJERCICIO43
+nro_de_campaña=19
+type_nro_de_campaña=type(nro_de_campaña)
+
+nro_de_campaña_es_int=isinstance(nro_de_campaña,int)
+nro_de_campaña_es_str=isinstance(nro_de_campaña,str)
+
+print("variable nro de campaña es:",type_nro_de_campaña)
+print("variable es int?:",nro_de_campaña_es_int)
+print("variable es str?:",nro_de_campaña_es_str)
+
+#EJERCICIO44
+consultora="Diana"
+type_consultora=type(consultora)
+
+consultora_es_str=isinstance(consultora,str)
+consultora_es_float=isinstance(consultora,float)
+
+print("variable consultora es:",type_consultora)
+print("variable es str?:",consultora_es_str)
+print("variable es float?:",consultora_es_float)
+
+#EJERCICIO45
+empresa="Avon"
+type_empresa=type(empresa)
+
+empresa_es_str=isinstance(empresa,str)
+empresa_es_bool=isinstance(empresa,bool)
+
+print("variable empresa es:",type_empresa)
+print("variable es str?:",empresa_es_str)
+print("variable es bool?:",empresa_es_bool)
+
+#EJERCICIO46
+producto_para_construccion="Cemento"
+type_producto_para_construccion=type(producto_para_construccion)
+
+producto_para_construccion_es_str=isinstance(producto_para_construccion,str)
+producto_para_construccion_es_int=isinstance(producto_para_construccion,int)
+
+print("variable producto para contruccion es:",type_producto_para_construccion)
+print("variable es str?:",producto_para_construccion_es_str)
+print("variable es int?:",producto_para_construccion_es_int)
+
+#EJERCICIO47
+nro_de_dientes_curados=3
+type_nro_de_dientes_curados=type(nro_de_dientes_curados)
+
+nro_de_dientes_curados_es_int=isinstance(nro_de_dientes_curados,int)
+nro_de_dientes_curados_es_float=isinstance(nro_de_dientes_curados,float)
+
+print("variable nro de dientes curados es:",type_nro_de_dientes_curados)
+print("variable es int?:",nro_de_dientes_curados_es_int)
+print("variable es float?:",nro_de_dientes_curados_es_float)
+
+#EJERCICIO48
+nro_de_dientes_extraidos=2
+type_nro_de_dientes_extraidos=type(nro_de_dientes_extraidos)
+
+nro_de_dientes_extraidos_es_int=isinstance(nro_de_dientes_extraidos,int)
+nro_de_dientes_extraidos_es_str=isinstance(nro_de_dientes_extraidos,str)
+
+print("variable nro de dientes extraidos es:",type_nro_de_dientes_extraidos)
+print("variable es int?:",nro_de_dientes_extraidos_es_int)
+print("variable es str?:",nro_de_dientes_extraidos_es_str)
+
+#EJERCICIO49
+costo_por_diente_curado=15.00
+type_costo_por_diente_curado=type(costo_por_diente_curado)
+
+costo_por_diente_curado_es_float=isinstance(costo_por_diente_curado,float)
+costo_por_diente_curado_es_str=isinstance(costo_por_diente_curado,str)
+
+print("variable costo por diente curado es:",type_costo_por_diente_curado)
+print("variable es float?:",costo_por_diente_curado_es_float)
+print("variable es str?:",costo_por_diente_curado_es_str)
+
+#EJERCICIO50
+costo_por_diente_extraido=25.00
+type_costo_por_diente_extraido=type(costo_por_diente_extraido)
+
+costo_por_diente_extraido_es_float=isinstance(costo_por_diente_extraido,float)
+costo_por_diente_extraido_es_bool=isinstance(costo_por_diente_extraido,bool)
+
+print("variable costo por diente extraido es:",type_costo_por_diente_extraido)
+print("variable es float?:",costo_por_diente_extraido_es_float)
+print("variable es bool?:",costo_por_diente_extraido_es_bool)
